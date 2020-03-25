85dcb48edd079b7bacccf98cd5a2a86f8e8c1186
Özgür İnce <ozgur@ince.ist>
2020-03-25 20:45:42 +0300

    modify 21

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float speed = 2;
+
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            body.velocity = new Vector2(body.velocity.x, speed);
+        
+    }
+}
