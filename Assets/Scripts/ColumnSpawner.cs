2d25b2c7c6a40d8dd22a7323804df6357c32f755
Özgür İnce <ozgur@ince.ist>
2020-03-26 03:35:40 +0300

    modify 24

---
+++ Assets/Scripts/ColumnSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject column;
+    [SerializeField] float maxY;
+    [SerializeField] float minY;
+    float lastTime;
+    float rnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rnd = Random.Range(2, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastTime > rnd)
+        {
+            lastTime = Time.time;
+            InstantiateColumn();
+            rnd = Random.Range(2, 4);
+        }
+    }
+
+    void InstantiateColumn()
+    {
+        var newColumn = Instantiate(column);
+        newColumn.transform.position = new Vector2(transform.position.x, Random.Range(minY, maxY));
+    }
+}
--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     [SerializeField] float speed;
+    float width;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (CompareTag("Ground"))
+            width = GetComponent<BoxCollider2D>().size.x;
+        else
+            width = -(GameManager.bottomLeft.x - GameObject.FindGameObjectWithTag("Pipe").GetComponent<BoxCollider2D>().size.x);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-        if (transform.position.x < -1.68f)
-            transform.position = new Vector2(transform.position.x + 2 * 1.68f, transform.position.y);
+        if (transform.position.x < -width)
+            if (CompareTag("Ground"))
+                transform.position = new Vector2(transform.position.x + 2 * width, transform.position.y);
+            else if (CompareTag("Column"))
+                Destroy(gameObject);
     }
 }
