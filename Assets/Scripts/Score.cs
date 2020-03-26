fa1eda41b2df7cf46f7050acabcd8dedb2c2044d
Özgür İnce <ozgur@ince.ist>
2020-03-26 19:32:20 +0300

    modify 29

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,19 +7,32 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     int score;
+    int highScore;
     Text text;
+
+    [SerializeField] Text panelScore;
+    [SerializeField] Text panelHighScore;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
         SetScore(0);
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+        panelHighScore.text = highScore.ToString();
     }
 
     private void SetScore(int v)
     {
         score = v;
         text.text = score.ToString();
+        panelScore.text = text.text;
+        if (score > highScore)
+        {
+            highScore = score;
+            panelHighScore.text = highScore.ToString();
+            PlayerPrefs.SetInt("highScore", highScore);
+        }
     }
 
     // Update is called once per frame
